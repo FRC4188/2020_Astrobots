c388d6d8514fc3b7df25bb5ca26d8bfc04bc320e
Aiden <aidenatchs@gmail.com>
2020-11-12 18:07:22 -0500

    Maintains direction ratios.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerOp.java
@@ -4,12 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "lol")
 public class ControllerOp extends OpMode {
 
-    private DcMotor frMotor, flMotor, brMotor, blMotor, intakeMotor;
+    private DcMotor frMotor, flMotor, brMotor, blMotor, intakeMotor, shooterMotor;
+    private Servo arm;
 
     @Override
     public void init() {
@@ -18,6 +20,8 @@ public class ControllerOp extends OpMode {
         brMotor = hardwareMap.get(DcMotor.class, "brMotor");
         blMotor = hardwareMap.get(DcMotor.class, "blMotor");
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        shooterMotor = hardwareMap.get(DcMotor.class, "shooterMotor");
+        //arm = hardwareMap.get(Servo.class, "arm");
 
         frMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         brMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -25,37 +29,55 @@ public class ControllerOp extends OpMode {
         blMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooterMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         //runtime.reset();
     }
 
     @Override
     public void loop() {
-        double forwardController = gamepad1.left_stick_y;
-        double strafeController = gamepad1.left_stick_x;
+        double forwardController = Math.pow(gamepad1.left_stick_y, 3);
+        double strafeController = Math.pow(gamepad1.left_stick_x, 3);
         double rotationController = gamepad1.right_stick_x;
 
         boolean isIntakePress = gamepad1.a;
+        boolean isOuttakePress = gamepad1.b;
+        float isShooterPress = gamepad1.right_trigger;
 
         if (isIntakePress) {
-            intakeMotor.setPower(0.5);
+            intakeMotor.setPower(1);
+        } else if(isOuttakePress) {
+            intakeMotor.setPower(-1);
         }
         else {
             intakeMotor.setPower(0);
         }
 
+       /* if(gamepad1.x) {
+            arm.setPosition(0);
+        }
 
-        forwardController = Range.clip(forwardController, -1, 1);
-        strafeController = Range.clip(strafeController, -1, 1);
-        rotationController = Range.clip(rotationController, -1, 1);
+        if(gamepad1.y) {
+            arm.setPosition(1);
+        }
+        */
 
         drivetrain(forwardController, strafeController, rotationController);
     }
 
     private void drivetrain(double forward, double strafe, double rotation) {
-        frMotor.setPower(forward - strafe - rotation);
-        flMotor.setPower(forward + strafe + rotation);
-        brMotor.setPower(forward + strafe - rotation);
-        blMotor.setPower(forward - strafe + rotation);
+        double frPower = forward - strafe - rotation;
+        double flPower = forward + strafe + rotation;
+        double brPower = forward + strafe - rotation;
+        double blPower = forward - strafe + rotation;
+
+        double maxPower = Math.max(Math.abs(frPower), Math.abs(flPower));
+        maxPower = Math.max(maxPower, Math.abs(brPower));
+        maxPower = Math.max(maxPower, Math.abs(blPower));
+
+        frMotor.setPower(frPower / maxPower);
+        flMotor.setPower(flPower / maxPower);
+        brMotor.setPower(brPower / maxPower);
+        blMotor.setPower(blPower / maxPower);
     }
 }
